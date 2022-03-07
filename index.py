957612f939b9fbd18efb296996c952424be360d8
copidopi <75416947+copidopi@users.noreply.github.com>
2022-03-07 20:44:42 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,39 +1,38 @@
 import requests
 from flask import Flask, render_template, request, redirect, session
-OAUTH2_CLIENT_ID = '949418697813008384'
-OAUTH2_CLIENT_SECRET = 'Jn-wM-IuXx7_SwCySD_pPoNf27QI3EqN'
-authURL = 'https://discord.com/api/oauth2/authorize'
-tokenURL = 'https://discord.com/api/oauth2/token'
-apiURLBase = 'https://discord.com/api/users/@me'
-revokeURL = 'https://discord.com/api/oauth2/token/revoke'
+client_id, client_secret = '1234', 'secret' #Client id, client secret
+oauth_url = "your oauth2 url"
 app = Flask(__name__)
-app.secret_key = "NZrEgg9FjFDHEI463G0d"
+app.secret_key = "cat" # You secret app key
 def access_token(code):
 	payload = {
-		"client_id": OAUTH2_CLIENT_ID,
-		"client_secret": OAUTH2_CLIENT_SECRET,
+		"client_id": client_id,
+		"client_secret": client_secret,
 		"grant_type": "authorization_code",
 		"code": code,
-		"redirect_uri": "http://127.0.0.1:8080/login",
+		"redirect_uri": "http://127.0.0.1:8080/login", #your redirect uri
 		"scope": "identify"
 	}
-	return requests.post(url=tokenURL, data=payload).json()["access_token"]
+	return requests.post("https://discord.com/api/oauth2/token", data=payload).json()["access_token"]
 @app.route("/")
 def index():
-	try:
+	if 'user' in session:
 		return render_template('authorization.html', username=session['user']['username'], discriminator=session['user']['discriminator'])
-	except KeyError:
-		return render_template('index.html')
+	return render_template('index.html', url=oauth_url)
 @app.route("/login")
 def login():
-	code = request.args.get('code')
-	try:
+	if 'user' not in session: 
+		code = request.args.get('code')
 		headers = {"authorization": f"Bearer {access_token(code)}"}
-		user_object = requests.get(url=apiURLBase, headers=headers).json()
+		user_object = requests.get("https://discord.com/api/users/@me", headers=headers).json()
 		session['user'] = user_object
 		return redirect('/')
-	except Exception:
-		return redirect('/')
-	
+	return "You are already logged in."
+@app.route("/logout")
+def logout():
+	if 'user' in session:
+		session.pop('user')
+		return "You have successfully logged out"
+	return "you are not authorized.", 401
 if __name__ == "__main__":
-	app.run(host='127.0.0.1', port=8080)
+	app.run(debug=True, host='127.0.0.1', port=8080)
